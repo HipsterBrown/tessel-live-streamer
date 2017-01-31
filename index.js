54b989cd61326a05f852b4b20dc2e92836415c54
HipsterBrown <headhipster@hipsterbrown.com>
2017-01-31 16:56:09 -0500

    initial update

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
-var io = require('socket.io')(server);
+// var io = require('socket.io')(server);
 var os = require('os');
 var path = require('path');
 var Five = require('johnny-five');
 var tesselIO = require('tessel-io');
-var port = 8080;
+var port = 80;
 
 var av = require('tessel-av');
+var camera = new av.Camera({
+  fps: 60
+});
 
 
 /*
@@ -19,22 +22,22 @@ var av = require('tessel-av');
  - maybe mount the camera on a servo to control the direction of the camera via toggle or buttons?
 */
 
+app.use(express.static(path.join(__dirname, '/public')));
+app.get('/stream', (request, response) => {
+  response.redirect(camera.url);
+});
+
 server.listen(port, function () {
   console.log(`http://${os.hostname()}.local:${port}`);
 });
 
-app.use(express.static(path.join(__dirname, '/public')));
-
+/*
 io.on('connection', (socket) => {
-  var camera = new av.Camera({
-    width: 640,
-    height: 480
-  });
-
   socket.on('ready', () => camera.stream());
   camera.on('data', (data) => {
     socket.emit('image', data.toString('base64'));
   });
 });
+*/
 
 process.on("SIGINT", _ => server.close());
